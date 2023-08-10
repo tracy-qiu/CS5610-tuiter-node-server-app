64b3cabd438fbc52dccec9266f49123b8af45414
Tracy Qiu <qiu.tr@northeastern.edu>
2023-08-10 18:55:00 -0400

    add dislikes to tuit objects

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -11,6 +11,7 @@ export default [
     likes: 2345,
     replies: 123,
     retuits: 432,
+    dislikes: 32,
     tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
   },
   {
@@ -24,6 +25,7 @@ export default [
     likes: 2345,
     replies: 321,
     retuits: 432,
+    dislikes: 90,
     title: "The Boring Company fixing traffic",
     tuit: "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
   },
@@ -37,8 +39,9 @@ export default [
     title: "Neurolink fixes paralisis and blindness",
     tuit: "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
     liked: true,
-    likes: 3456,
+    likes: 3458,
     replies: 345,
     retuits: 654,
+    dislikes: 12,
   },
 ];
